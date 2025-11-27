9df6a502224a9a40c5a9b6dbd51f724bcb0c2a94
Your Name <you@example.com>
2025-11-27 09:00:35 +0500

    fix ci cd

--- logistic/serializers.py
+++ logistic/serializers.py
@@ -9,9 +9,9 @@ class ProductSerializer(serializers.ModelSerializer):
         fields = ['id', 'title', 'description']
 
 
-
 class ProductPositionSerializer(serializers.ModelSerializer):
     product = ProductSerializer()
+
     class Meta:
         model = StockProduct
         fields = ['product', 'quantity', 'price']
@@ -24,29 +24,16 @@ class StockSerializer(serializers.ModelSerializer):
         model = Stock
         fields = ['id', 'address', 'positions']
 
-
     def create(self, validated_data):
-        # достаем связанные данные для других таблиц
         positions = validated_data.pop('positions')
-        # создаем склад по его параметрам
         stock = super().create(validated_data)
-        # здесь вам надо заполнить связанные таблицы
-        # в нашем случае: таблицу StockProduct
-        # с помощью списка positions
         for position_data in positions:
             StockProduct.objects.create(stock=stock, **position_data)
         return stock
 
     def update(self, instance, validated_data):
-        # достаем связанные данные для других таблиц
         positions = validated_data.pop('positions')
-
-        # обновляем склад по его параметрам
         stock = super().update(instance, validated_data)
-
-        # здесь вам надо обновить связанные таблицы
-        # в нашем случае: таблицу StockProduct
-        # с помощью списка positions
         for position in positions:
             StockProduct.objects.update_or_create(
                 stock=stock,
